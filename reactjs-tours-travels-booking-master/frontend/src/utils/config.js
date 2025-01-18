c43e7c432d0dbceb3598898934e171897b342fb8
PixelPioneer2003 <154107788+PixelPioneer2003@users.noreply.github.com>
2025-01-18 10:13:56 +0530

    Update config.js

--- reactjs-tours-travels-booking-master/frontend/src/utils/config.js
+++ reactjs-tours-travels-booking-master/frontend/src/utils/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:3000/api/v1'
+// export const BASE_URL = 'http://localhost:3000/api/v1'
+export const BASE_URL='https://travelworld-backend-zjjm.onrender.com'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
